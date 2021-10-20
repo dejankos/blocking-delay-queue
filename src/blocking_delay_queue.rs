2e60b89722869c5b2b9cc3747c61ff9643ef99a2
dejankos <kosdejan@yahoo.com>
2021-10-20 21:23:35 +0200

    smth

--- src/blocking_delay_queue.rs
+++ src/blocking_delay_queue.rs
@@ -1,6 +1,6 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
-use std::sync::{Condvar, Mutex};
+use std::sync::{Condvar, Mutex, MutexGuard};
 use std::time::Duration;
 
 use crate::delay_item::Delayed;
@@ -11,9 +11,7 @@ type MinHeap<T> = BinaryHeap<Reverse<T>>;
 trait DelayQueue<T>
     where T: Delayed
 {
-    fn add(&self, e: T) -> bool;
-
-    fn clear(&self);
+    fn add(&self, e: T);
 
     fn offer(&self, e: T, timeout: Duration) -> bool;
 
@@ -26,30 +24,84 @@ trait DelayQueue<T>
     fn remove(&self, e: T) -> bool;
 
     fn size(&self) -> usize;
+
+    fn clear(&self);
 }
 
 pub struct BlockingDelayQueue<T>
 {
     heap: Mutex<MinHeap<T>>,
-    condvar: Condvar
+    condvar: Condvar,
 }
 
 impl<T> BlockingDelayQueue<T>
     where T: Delayed + Ord
 {
-    fn new_unbounded() -> Self {
+    pub fn new_unbounded() -> Self {
         BlockingDelayQueue {
             heap: Mutex::new(BinaryHeap::new()),
-            condvar: Condvar::new()
+            condvar: Condvar::new(),
         }
     }
 
-
-    fn new_with_capacity(capacity: usize) -> Self {
+    pub fn new_with_capacity(capacity: usize) -> Self {
         BlockingDelayQueue {
             heap: Mutex::new(BinaryHeap::with_capacity(capacity)),
-            condvar: Condvar::new()
+            condvar: Condvar::new(),
         }
+    }
+
+    fn heap_mutex(&self) -> MutexGuard<'_, MinHeap<T>> {
+        self.heap.lock().expect("Queue lock poisoned")
+    }
+}
+
+
+impl<T> DelayQueue<T> for BlockingDelayQueue<T>
+    where T: Delayed + Ord
+{
+    fn add(&self, e: T) {
+        let mut heap = self.heap_mutex();
+        if heap.len() < heap.capacity() {
+            heap.push(Reverse(e));
+        } else {
+            let cap = heap.capacity();
+            let mut mutex = self
+                .condvar
+                .wait_while(heap, |h| h.len() >= cap)
+                .expect("Queue lock poisoned");
+            mutex.push(Reverse(e));
+        }
+
+        self.condvar.notify_one()
+    }
+
+    fn offer(&self, _e: T, _timeout: Duration) -> bool {
+        todo!()
+    }
+
+    fn peek(&self) -> Option<T> {
+        todo!()
+    }
+
+    fn take(&self) -> T {
+        todo!()
+    }
+
+    fn poll(&self, _timeout: Duration) -> Option<T> {
+        todo!()
+    }
+
+    fn remove(&self, _e: T) -> bool {
+        todo!()
+    }
+
+    fn size(&self) -> usize {
+        todo!()
+    }
+
+    fn clear(&self) {
+        self.heap_mutex().clear();
     }
 }
 
