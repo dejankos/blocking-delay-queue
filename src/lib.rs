3184e8bb283ee3c63da59fd6dca887a20c39b169
dejankos <kosdejan@yahoo.com>
2021-11-01 09:58:44 +0100

    lib root docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod blocking_delay_queue;
 mod delay_item;
 
+//! A thread safe blocking delay queue in which an element can only be taken when its delay has expired.
+
 pub use self::blocking_delay_queue::BlockingDelayQueue;
 pub use self::delay_item::{DelayItem, Delayed};
